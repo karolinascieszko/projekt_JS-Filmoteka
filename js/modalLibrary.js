5d0097f47e8ba7f1ebd0e8bbd2874c6428d05c26
GrzegorzHamala <81816000+GrzegorzHamala@users.noreply.github.com>
2021-10-25 11:59:36 +0200

    modal

--- js/modalLibrary.js
+++ js/modalLibrary.js
@@ -1,14 +1,24 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", escape);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function escape() {
+    refs.modal.classList.add("is-hidden");
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "Escape") {
+        refs.modal.classList.add("is-hidden");
+      }
+    });
+  }
+})();
